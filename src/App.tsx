03511ebac8d747803bd023b364eb3617ab5cef5d
tingfeng12 <1097864547@qq.com>
2022-04-04 18:28:24 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [title, getTitle] = useState<{ name?: string }>({});
+  useEffect(() => {
+    fetch("https://my-worker.10978645473607.workers.dev")
+      .then((response: Response) => response?.json())
+      .then((data) => {
+        console.log(data);
+        data = {'name':data}
+        getTitle({ ...data });
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>hahhahahah</h2>
+      {title.name}
     </div>
   );
 }
